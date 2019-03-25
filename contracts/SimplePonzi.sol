c1a8998478f609ad3d6f6010554d55932ca0ebf2
Carlos Oporto <carlosoporto@gmail.com>
2019-03-25 21:17:34 +0100

    Update SimplePonzi.sol

---
+++ contracts/SimplePonzi.sol
@@ -0,0 +1,20 @@
+pragma solidity ^0.5.0;
+
+contract SimplePonzi {
+  address public currentBacker;
+  uint public currentContribution = 0;
+
+  function () payable public {
+    // new investments must be 10% higher than the current investment
+    uint minimumContruibution = currentContribution * 11/10;
+    require(msg.value > minimumContruibution);
+
+    // document new backer
+    address previousBacker = currentBacker;
+    currentBacker = msg.sender;
+    currentContribution = msg.value;
+
+    // payout for previous backer
+    previousBacker.send(msg.value);-
+  }
+}
